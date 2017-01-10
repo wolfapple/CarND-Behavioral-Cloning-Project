80be1f009e63606171f90399f7b305f880c56283
barney-kim <barney.kim@kakaocorp.com>
2017-01-10 18:57:49 +0900

    preprocessing

--- drive.py
+++ drive.py
@@ -19,6 +19,7 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 import tensorflow as tf
 tf.python.control_flow_ops = tf
 
+from model import preprocess
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -36,7 +37,7 @@ def telemetry(sid, data):
     # The current image from the center camera of the car
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
+    image_array = preprocess(np.asarray(image))
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.image as mpimg
+import cv2
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -8,54 +9,77 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
 from keras.layers.convolutional import Convolution2D
 
+def rgb_clahe(bgr_img,limit=3,grid=4):
+    b,g,r = cv2.split(bgr_img)
+    clahe = cv2.createCLAHE(clipLimit=limit, tileGridSize=(grid,grid))
+    b = clahe.apply(b)
+    g = clahe.apply(g)
+    r = clahe.apply(r)
+    return cv2.merge([b,g,r])
+
+def preprocess(img):
+  roi = img[70:130, :, :]
+  resize = cv2.resize(roi, (64, 64), interpolation=cv2.INTER_AREA)
+  clahe = rgb_clahe(resize)
+  return clahe
+
 def generate_batch(data, batch_size=128):
-  images = np.zeros((batch_size, 160, 320, 3))
+  images = np.zeros((batch_size, 64, 64, 3))
   steerings = np.zeros(batch_size)
   total = len(data)
   current = 0
   while True:
     for i in range(batch_size):
       row = data.iloc[current]
-      images[i] = mpimg.imread('data/'+row.center)
+      images[i] = preprocess(mpimg.imread('data/'+row.center))
       steerings[i] = row.steering
       current = (current + 1) % total
     yield images, steerings
 
-# read csv
-headers = ['center','left','right','steering','throttle','brake','speed']
-csv = pd.read_csv('data/driving_log.csv', names=headers, skiprows=1)
+def read_csv(path):
+  headers = ['center','left','right','steering','throttle','brake','speed']
+  return pd.read_csv(path, names=headers, skiprows=1)
 
-# split data
-train_nonzero = csv[csv.steering != 0]
-train_zero = (csv[csv.steering == 0]).sample(frac=.1)
-train = pd.concat([train_nonzero, train_zero], ignore_index=True)
-train_data, valid_data = train_test_split(shuffle(train), test_size=0.2, random_state=1234)
+def split_data(data):
+  train_nonzero = csv[csv.steering != 0]
+  train_zero = (csv[csv.steering == 0]).sample(frac=.1)
+  train = pd.concat([train_nonzero, train_zero], ignore_index=True)
+  return train_test_split(shuffle(train), test_size=0.2, random_state=1234)
 
-# model
-model = Sequential()
-model.add(Lambda(lambda x: x/127.5 - 1.,
-          input_shape=(160, 320, 3),
-          output_shape=(160, 320, 3)))
-model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
-model.add(ELU())
-model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
-model.add(ELU())
-model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
-model.add(Flatten())
-model.add(Dropout(.2))
-model.add(ELU())
-model.add(Dense(512))
-model.add(Dropout(.5))
-model.add(ELU())
-model.add(Dense(1))
+def get_model():
+  model = Sequential()
+  model.add(Lambda(lambda x: x/127.5 - 1.,
+            input_shape=(64, 64, 3),
+            output_shape=(64, 64, 3)))
+  model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+  model.add(ELU())
+  model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+  model.add(ELU())
+  model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+  model.add(Flatten())
+  model.add(Dropout(.2))
+  model.add(ELU())
+  model.add(Dense(512))
+  model.add(Dropout(.5))
+  model.add(ELU())
+  model.add(Dense(1))
+  return model
 
-model.summary()
+if __name__ == '__main__':
+  # prepare data
+  csv = read_csv('data/driving_log.csv')
+  train_data, valid_data = split_data(csv)
 
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+  # get model
+  model = get_model()
+  model.summary()
 
-model.fit_generator(generate_batch(train_data), verbose=1, samples_per_epoch=len(train_data), nb_epoch=5,
-  validation_data=generate_batch(valid_data), nb_val_samples=len(valid_data))
+  # training
+  model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+  model.fit_generator(generate_batch(train_data), verbose=1, samples_per_epoch=3328, nb_epoch=5,
+    validation_data=generate_batch(valid_data), nb_val_samples=896)
 
-model.save_weights('model.h5')
-with open('model.json', 'w') as f:
-  f.write(model.to_json())
+  # model save
+  model.save_weights('model.h5')
+  with open('model.json', 'w') as f:
+    f.write(model.to_json())
